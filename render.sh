f03a95a3f33b9413eb7909949bedb0945abdf9d2
Gregor Sturm <mail@gregor-sturm.de>
2019-11-04 08:42:25 +0100

    init

---
+++ render.sh
@@ -0,0 +1,3 @@
+#Rscript -e "rmarkdown::render('nb_dir/notebook.Rmd', run_pandoc=FALSE, clean=FALSE)"
+Rscript -e "rmarkdown::render('nb_dir/notebook.Rmd', knit_root_dir='/home/sturm/Downloads/test_rmd/work_dir')"
+#Rscript -e "rmarkdown::render('nb_dir/notebook.Rmd', run_pandoc=FALSE, clean=FALSE, knit_root_dir='/home/sturm/Downloads/test_rmd/work_dir')"
